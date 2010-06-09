fcaf08e847cb1ad393db1fa33b91bbf7db61a3e7
Alexander Chuprin <a.s.chuprin@gmail.com>
2010-06-09 23:12:51 +0400

    Remove unused code

--- content.js
+++ content.js
@@ -2,9 +2,8 @@ document.addEventListener("beforeload", handleBeforeLoadEvent, true);
 
 function handleBeforeLoadEvent(event)
 {
-	const element = event.target;
-    var url = element.ownerDocument.URL;
-
+    var url = window.location.href;
+    
     if ( /feed:\/\//.test(url)) {
         window.location = 'http://www.google.com/ig/add?feedurl=' + encodeURI(url.replace('feed://', 'http://'));
     }
