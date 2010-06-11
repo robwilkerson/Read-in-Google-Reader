186f5eef3f5437cc30f319c10e5c5bfa39c6df7a
Rob Wilkerson <rob@robwilkerson.org>
2010-06-11 09:03:16 -0400

    Sending a new feed directly to Google Reader, bypassing the selection of Reader or iGoogle.

--- content.js
+++ content.js
@@ -5,6 +5,6 @@ function handleBeforeLoadEvent(event)
     var url = window.location.href;
     
     if ( /feed:\/\//.test(url)) {
-        window.location = 'http://www.google.com/ig/add?feedurl=' + encodeURI(url.replace('feed://', 'http://'));
+        window.location = 'http://www.google.com/ig/addtoreader?feedurl=' + encodeURI( url.replace( 'feed://', 'http://' ) );
     }
 }
