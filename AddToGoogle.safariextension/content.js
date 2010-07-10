9f1731a1c188a11b659c148e65838484762307a1
Rob Wilkerson <rob@robwilkerson.org>
2010-07-09 21:56:29 -0400

    When attempting to load a page under SSL, the extension wasn't loading. Additionally, the feed:// protocol wasn't being replaced properly (under ssl, the protocol presents as feed:https:// rather than a simple feed://). Fixes #3.

--- AddToGoogle.safariextension/content.js
+++ AddToGoogle.safariextension/content.js
@@ -1,27 +1,30 @@
-document.addEventListener( 'beforeload', getSettings, true );
-
-var settings = null;
-var url      = window.location.href;
-
-function getSettings() {
-	if( /feed:\/\//.test( url ) && !settings ) {
-		settings = safari.self.tab.canLoad( event );
-		
-		console.log( 'Settings returned to content.js' );
-		console.log( settings );
-		
-		console.log( 'Redirecting...' );
-		redirect();
+if( window === top.window ) {
+	document.addEventListener( 'beforeload', getSettings, true );
+	
+	var settings = null;
+	var url      = window.location.href;
+	var feed_url = url.replace( /^feed:(http(s):)?\/\//, 'http$2://' );
+	
+	function getSettings() {
+		if( /^feed:(https:)?\/\//.test( url ) && !settings ) {
+			settings = safari.self.tab.canLoad( event );
+			
+			console.log( 'Settings returned to content.js' );
+			console.log( settings );
+			
+			console.log( 'Redirecting...' );
+			redirect();
+		}
 	}
-}
-
-function redirect() {
-	if( !settings.igoogle ) {
-		console.log( 'Bypassing iGoogle' );
-		window.location = 'http://www.google.com/ig/addtoreader?feedurl=' + encodeURI( url.replace( 'feed://', 'http://' ) );
-	}
-	else {
-		console.log( 'Displaying the iGoogle option' );
-		window.location = 'http://www.google.com/ig/add?feedurl=' + encodeURI(url.replace('feed://', 'http://'));
+	
+	function redirect() {
+		if( !settings.igoogle ) {
+			console.log( 'Bypassing iGoogle' );
+			window.location = 'http://www.google.com/ig/addtoreader?feedurl=' + encodeURI( feed_url );
+		}
+		else {
+			console.log( 'Displaying the iGoogle option' );
+			window.location = 'http://www.google.com/ig/add?feedurl=' + encodeURI( feed_url );
+		}
 	}
 }
