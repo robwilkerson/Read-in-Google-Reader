580a7498c92129ec190f455320afb60ce10ebae4
Rob Wilkerson <rob@robwilkerson.org>
2010-07-11 17:35:56 -0400

    Optimizing by moving most of the work into the global HTML file where settings are easily accessible. This way, no return message is required.

--- AddToGoogle.safariextension/content.js
+++ AddToGoogle.safariextension/content.js
@@ -1,30 +1,8 @@
-if( window === top.window ) {
-	document.addEventListener( 'beforeload', getSettings, true );
+if( window === window.top ) {
+	// console.log( 'Thinking about subscribing' );
 	
-	var settings = null;
-	var url      = window.location.href;
-	var feed_url = url.replace( /^feed:(http(s):)?\/\//, 'http$2://' );
-	
-	function getSettings() {
-		if( /^feed:(https:)?\/\//.test( url ) && !settings ) {
-			settings = safari.self.tab.canLoad( event );
-			
-			console.log( 'Settings returned to content.js' );
-			console.log( settings );
-			
-			console.log( 'Redirecting...' );
-			redirect();
-		}
-	}
-	
-	function redirect() {
-		if( !settings.igoogle ) {
-			console.log( 'Bypassing iGoogle' );
-			window.location = 'http://www.google.com/ig/addtoreader?feedurl=' + encodeURI( feed_url );
-		}
-		else {
-			console.log( 'Displaying the iGoogle option' );
-			window.location = 'http://www.google.com/ig/add?feedurl=' + encodeURI( feed_url );
-		}
+	if( /^feed:/.test( window.location.href ) ) {
+		// console.log( 'It\'s a feed. Dispatching the subscribe message' );
+		safari.self.tab.dispatchMessage( 'subscribe', window.location.href );
 	}
 }
